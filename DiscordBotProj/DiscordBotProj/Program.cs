c2d10cde74c4967fb9057469befaa26e166c6bcc
Nikita Zaytsev <nikitos945max@gmail.com>
2019-09-06 15:35:46 +0300

    init proj

---
+++ DiscordBotProj/DiscordBotProj/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+
+namespace DiscordBotProj
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            Console.WriteLine("In method!");
+        }
+    }
+}
