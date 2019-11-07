e703cdfb17efc1b096bc40267724edd948587ac7
Nikita Zaytsev <nikitos945max@gmail.com>
2019-11-07 06:49:57 +0300

    чот происходит

--- DiscordBotProj/DiscordBotProj/Program.cs
+++ DiscordBotProj/DiscordBotProj/Program.cs
@@ -1,16 +1,30 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace DiscordBotProj
 {
     public class Program
     {
+        private DiscordSocketClient _client;
+
         public static void Main(string[] args)
             => new Program().MainAsync().GetAwaiter().GetResult();
 
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
         public async Task MainAsync()
         {
-            Console.WriteLine("In method!");
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+            await _client.LoginAsync(TokenType.Bot, "NjE5NTA4ODIxNzA2MTQ1ODAz.XcLeaQ.V7lSnMyey8T8j0-bm8lUOJI9lN4");
+            await _client.StartAsync();
+            await Task.Delay(-1);
         }
     }
 }
